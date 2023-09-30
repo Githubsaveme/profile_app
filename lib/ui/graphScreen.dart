05a8f9d0b521eecf32e2c1318d2523fc44707b76
potato <potatoesking3333@gmail.com>
2023-09-30 16:02:38 +0530

    new Added

--- lib/ui/graphScreen.dart
+++ lib/ui/graphScreen.dart
@@ -1,6 +1,7 @@
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class GraphScreen extends StatefulWidget {
   const GraphScreen({super.key});
@@ -33,57 +34,130 @@ class _GraphScreenState extends State<GraphScreen> {
     SalesData('Mar', 20),
     SalesData('Aug', 31),
   ];
+  late TooltipBehavior _tooltipBehavior;
 
   @override
   void initState() {
     super.initState();
     yearFunction();
+    _tooltipBehavior = TooltipBehavior(enable: true);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Graph'), actions: [
-        DropdownButtonHideUnderline(
-          child: DropdownButton2(
-              hint: Text(dropValue.isNotEmpty ? dropValue : 'Select year'),
-              style: const TextStyle(
-                  color: Colors.black, fontWeight: FontWeight.w500),
-              items: yearModelList
-                  .map((e) => DropdownMenuItem(
-                        value: e.yearName,
-                        child: Text(
-                          e.yearName,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ))
-                  .toList(),
-              onChanged: (String? value) {
-                dropValue = value!;
-                isSelected = !isSelected;
-                setState(() {});
-              }),
-        )
-      ]),
-      body: showCharts(),
+    var size = MediaQuery.sizeOf(context);
+    return Container(
+      width: size.width,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.grey,
+            Colors.black.withOpacity(0.8),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: size.width * 0.02),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: size.width * 0.1),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {}, child: const Text("Load Data")),
+                    const Expanded(child: SizedBox()),
+                    ElevatedButton(
+                        onPressed: () {}, child: const Text("Plot Data"))
+                  ],
+                ),
+                showCharts(),
+                DropdownButtonHideUnderline(
+                  child: DropdownButton2(
+                      hint: Text(
+                        dropValue.isNotEmpty ? dropValue : 'Select year',
+                        style: const TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.normal),
+                      ),
+                      style: const TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w500),
+                      items: yearModelList
+                          .map((e) => DropdownMenuItem(
+                                value: e.yearName,
+                                child: Text(
+                                  e.yearName,
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ))
+                          .toList(),
+                      onChanged: (String? value) {
+                        dropValue = value!;
+                        isSelected = !isSelected;
+                        setState(() {});
+                      }),
+                ),
+                showLineChart(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 
   Widget showCharts() {
     return SfCartesianChart(
-        backgroundColor: Colors.red.withOpacity(0.05),
+        plotAreaBorderColor: Colors.yellowAccent,
+        enableAxisAnimation: true,
         primaryXAxis: CategoryAxis(),
+        // legend: const Legend(isVisible: true),
         primaryYAxis: NumericAxis(minimum: 0, maximum: 40, interval: 10),
         series: <ChartSeries<SalesData, String>>[
           ColumnSeries<SalesData, String>(
+            dataLabelSettings:
+                DataLabelSettings(color: Colors.yellowAccent.shade200),
+            color: Colors.yellowAccent.shade100,
             dataSource: isSelected ? chartData : chartData2,
             xValueMapper: (SalesData sales, _) => sales.year,
             yValueMapper: (SalesData sales, _) => sales.sales,
           ),
+        ]);
+  }
+
+  Widget showLineChart() {
+    return SfCartesianChart(
+        primaryXAxis: CategoryAxis(
+            arrangeByIndex: true, autoScrollingMode: AutoScrollingMode.start),
+        // Chart title
+        title: ChartTitle(
+            text: 'Half yearly sales analysis',
+            textStyle: const TextStyle(
+              color: Colors.yellowAccent,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w700,
+            )),
+        // Enable legend
+        //legend: const Legend(isVisible: true),
+        // Enable tooltip
+        tooltipBehavior: _tooltipBehavior,
+        series: <LineSeries<SalesData, String>>[
+          LineSeries<SalesData, String>(
+              color: Colors.yellowAccent,
+              dataSource: isSelected ? chartData : chartData2,
+              xValueMapper: (SalesData sales, _) => sales.year,
+              yValueMapper: (SalesData sales, _) => sales.sales,
+              // Enable data label
+              dataLabelSettings: const DataLabelSettings(
+                  isVisible: true, color: Colors.yellowAccent))
         ]);
   }
 
--- lib/ui/profileScreen.dart
+++ lib/ui/profileScreen.dart
@@ -1,6 +1,7 @@
-import 'dart:io';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:profile_app/ui/graphScreen.dart';
 
 class MyProfileScreen extends StatefulWidget {
@@ -11,212 +12,94 @@ class MyProfileScreen extends StatefulWidget {
 }
 
 class _MyProfileScreenState extends State<MyProfileScreen> {
-  String title = "My Profile";
-  String buttonText = "Edit Profile";
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Scaffold(
-      bottomNavigationBar: Container(
-        margin: Platform.isIOS
-            ? const EdgeInsets.only(left: 20, right: 20, bottom: 30)
-            : EdgeInsets.zero,
-        width: size.width,
-        height: size.width * 0.13,
-        child: ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.redAccent,
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero)),
-            child: Text(
-              buttonText,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: size.width * 0.04),
-            )),
+    Timer(
+        const Duration(seconds: 5),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => const GraphScreen())));
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          tileMode: TileMode.clamp,
+          begin: Alignment.topCenter,
+          colors: [Colors.white30, Colors.white],
+        ),
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: Colors.transparent,
-            leading: InkWell(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Row(
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: size.width * 0.1,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'TDR Application',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: size.width * 0.08),
+                )
+                    .animate(
+                      delay: 1000.ms,
+                      onPlay: (controller) => controller.repeat(), // loop
+                    )
+                    .shimmer(delay: 500.ms),
+              ),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Icon(
-                    Icons.arrow_back_ios_rounded,
-                    color: Colors.white,
-                    size: size.width * 0.05,
-                  ),
-                  Text(
-                    title,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: size.width * 0.04,
-                        fontWeight: FontWeight.bold),
-                  )
+                  Container(
+                    padding: EdgeInsets.all(size.width * 0.1),
+                    decoration: const BoxDecoration(color: Colors.grey),
+                    child: const Text(
+                      "IIT BHU",
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  )..animate().slide().scale(),
+                  Container(
+                    padding: EdgeInsets.all(size.width * 0.1),
+                    decoration: const BoxDecoration(color: Colors.grey),
+                    child: const Text(
+                      "NCL",
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  ).animate().slide().scale()
                 ],
               ),
-            ),
-            leadingWidth: size.width * 0.35,
-            expandedHeight: size.height / 3.3,
-            collapsedHeight: 100,
-            floating: true,
-            pinned: true,
-            snap: true,
-            flexibleSpace: FlexibleSpaceBar(
-              collapseMode: CollapseMode.pin,
-              background: Stack(
+              const Spacer(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  ClipPath(
-                    clipper: RoundedClipper(),
-                    child: Container(
-                      height: size.height / 3,
-                      width: size.width,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.orange,
-                            Colors.orange, Colors.yellowAccent,
-                            const Color(0xFFCD3B3B).withOpacity(1),
-                            // Color(0xFFEE9260),
-                            // Color(0xFFCD3B3B),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Container(
-                          padding: EdgeInsets.all(size.width * 0.02),
-                          decoration: const BoxDecoration(
-                              color: Colors.white, shape: BoxShape.circle),
-                          child: ClipOval(
-                              child: Image.network(
-                            'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
-                            height: size.width * 0.36,
-                            width: size.width * 0.36,
-                            fit: BoxFit.cover,
-                          )))),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Container(
-                      padding: EdgeInsets.all(size.width * 0.02),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(size.width * 0.04)),
-                          color: Colors.red,
-                          shape: BoxShape.rectangle),
-                      child: CircleAvatar(
-                          radius: size.width * 0.05,
-                          backgroundColor: Colors.redAccent,
-                          child: IconButton(
-                              onPressed: () {
-                                Navigator.push(context,
-                                    MaterialPageRoute(builder: (context) {
-                                  return const GraphScreen();
-                                }));
-                              },
-                              icon: const Icon(
-                                Icons.auto_graph_outlined,
-                                color: Colors.white,
-                              ))),
-                    ),
-                  )
+                  Container(
+                      padding: EdgeInsets.all(size.width * 0.03),
+                      decoration: BoxDecoration(color: Colors.grey.shade300),
+                      child: const Text('Project PI :- Prof. S.K Sharma',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontStyle: FontStyle.italic))),
+                  SizedBox(height: size.width * 0.01),
+                  Container(
+                      padding: EdgeInsets.all(size.width * 0.03),
+                      decoration: BoxDecoration(color: Colors.grey.shade300),
+                      child: const Text('Research fellow :- Kadam Mishra',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontStyle: FontStyle.italic))),
                 ],
-              ),
-            ),
-          ),
-          SliverList(
-              delegate: SliverChildListDelegate([
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              keyboardType: TextInputType.name,
-              decoration: const InputDecoration(
-                  hintText: 'First name',
-                  prefixIcon: Icon(
-                    Icons.account_box_outlined,
-                    color: Colors.redAccent,
-                  )),
-            ),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              keyboardType: TextInputType.name,
-              decoration: const InputDecoration(
-                  hintText: 'Email Address',
-                  prefixIcon: Icon(
-                    Icons.email_outlined,
-                    color: Colors.redAccent,
-                  )),
-            ),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                  hintText: 'Mobile Number',
-                  prefixIcon: Icon(
-                    Icons.phone_android_sharp,
-                    color: Colors.redAccent,
-                  )),
-            ),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                  hintText: 'City',
-                  prefixIcon: Icon(
-                    Icons.location_city,
-                    color: Colors.redAccent,
-                  )),
-            ),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                  hintText: 'State',
-                  prefixIcon: Icon(
-                    Icons.location_city,
-                    color: Colors.redAccent,
-                  )),
-            ),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                  hintText: 'Address',
-                  prefixIcon: Icon(
-                    Icons.location_city,
-                    color: Colors.redAccent,
-                  )),
-            ),
-            SizedBox(
-              height: size.width * 0.15,
-            ),
-          ]))
-        ],
-      ),
+              )
+            ],
+          )),
     );
   }
-}
-
-class RoundedClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0.0, size.height - 100);
-    path.quadraticBezierTo(
-        size.width / 2, size.height, size.width, size.height - 100);
-    path.lineTo(size.width, 0.0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
